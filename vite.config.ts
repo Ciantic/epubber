d8d5b380ace70c9c6882866fad56b4ecd1cb1934
Jari Pennanen <ciantic@oksidi.com>
2021-10-25 01:14:15 +0300

    csp...

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,24 @@
-import { defineConfig } from "vite";
+import { defineConfig, Plugin } from "vite";
 import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig({
-    plugins: [solidPlugin()],
+    plugins: [
+        solidPlugin(),
+        {
+            name: "html-csp",
+            transform(src, id) {
+                if (id.endsWith("index.html")) {
+                    return {
+                        code: src.replace(
+                            "<!-- CSP -->",
+                            `<meta http-equiv="Content-Security-Policy" content="default-src 'self'; worker-src blob:;" />`
+                        ),
+                        map: null,
+                    };
+                }
+            },
+        } as Plugin,
+    ],
     base: "./",
     build: {
         target: "esnext",
